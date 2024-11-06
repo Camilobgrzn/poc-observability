c908ee850e727b3a144e917e05391e1ef179eb1d
Camilo Barreto <camilo.barreto.grzn@gmail.com>
2024-11-06 16:54:54 -0500

    feat(logging): Actualizar configuración de registro y telemetría en Procesos
    Eliminado el paquete 'Elastic.Serilog.Sinks' y añadidos nuevos paquetes para OpenTelemetry y 'Serilog.Sinks.Elasticsearch' en 'Procesos.csproj'. Actualizado 'Program.cs' para configurar Serilog con registro en consola y Elasticsearch. Configurado OpenTelemetry para trazas y métricas, incluyendo la configuración del exportador OTLP.

--- src/Procesos/Program.cs
+++ src/Procesos/Program.cs
@@ -1,8 +1,27 @@
-
 using MassTransit;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Procesos;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 IHost host = Host.CreateDefaultBuilder(args)
+    //Configure Serilog
+    .UseSerilog((context, configuration) =>
+    {
+        var elasticUri = Environment.GetEnvironmentVariable("ELASTIC_URI") ?? "http://localhost:9200";
+        configuration
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticUri))
+            {
+                IndexFormat = "applogs-{0:yyyy.MM}",
+                AutoRegisterTemplate = true,
+                AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+            })
+            .Enrich.WithProperty("Application", "Procesos");
+    })
     .ConfigureServices(services =>
     {
         services.AddMassTransit(x =>
@@ -15,6 +34,35 @@ IHost host = Host.CreateDefaultBuilder(args)
                 cfg.ConfigureEndpoints(context);
             });
         });
+
+        // Configure OpenTelemetry
+        var resourceBuilder = ResourceBuilder.CreateDefault()
+            .AddService("Procesos");
+
+        services.AddOpenTelemetry()
+            .WithTracing(tracerProviderBuilder =>
+            {
+                tracerProviderBuilder
+                    .SetResourceBuilder(resourceBuilder)
+                    .AddSource("MassTransit")
+                    .AddOtlpExporter(options =>
+                    {
+                        var elasticpApm = Environment.GetEnvironmentVariable("ELASTIC_APM_URI") ?? "http://localhost:8200";
+                        options.Endpoint = new Uri(elasticpApm);
+                    });
+            })
+            .WithMetrics(metricProviderBuilder =>
+            {
+                metricProviderBuilder
+                    .SetResourceBuilder(resourceBuilder)
+                    .AddRuntimeInstrumentation()
+                    .AddMeter("MassTransit")
+                    .AddOtlpExporter(options =>
+                    {
+                        var elasticpApm = Environment.GetEnvironmentVariable("ELASTIC_APM_URI") ?? "http://localhost:8200";
+                        options.Endpoint = new Uri(elasticpApm);
+                    });
+            });
 
     })
     .Build();
